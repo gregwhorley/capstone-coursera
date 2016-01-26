63fb24854f857f92048f4704013a2f259f9797f6
Greg Whorley <greg@whorley.com>
2016-01-25 21:50:02 -0800

    started on unit tests

--- src/test/TestFirstRatings.java
+++ src/test/TestFirstRatings.java
@@ -1,14 +1,20 @@
+package test;
+
 import coursera.capstone.project.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import junit.framework.*;
 import edu.duke.*;
 
+import java.util.ArrayList;
+
 public class TestFirstRatings {
     @Test
     public void testLoadMovies() {
-
-        ArrayList<Movie> movieArrayList = FirstRatings.
+        String filename = "..\\..\\data\\ratedmovies_short.csv";
+        FirstRatings firstRatings = new FirstRatings();
+        ArrayList<Movie> movieArrayList = firstRatings.loadMovies(filename);
+        assertEquals(5,movieArrayList.size());
 
     }
 }
