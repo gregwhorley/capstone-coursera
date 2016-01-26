f82107341ad02774cca08ed30e7d081a523683b6
Greg Whorley <greg@whorley.com>
2016-01-25 21:38:22 -0800

    another move of files

--- src/test/TestFirstRatings.java
+++ src/test/TestFirstRatings.java
@@ -2,10 +2,13 @@ import coursera.capstone.project.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import junit.framework.*;
+import edu.duke.*;
 
 public class TestFirstRatings {
     @Test
     public void testLoadMovies() {
-        FirstRatings firstRatings = new FirstRatings();
+
+        ArrayList<Movie> movieArrayList = FirstRatings.
+
     }
 }
