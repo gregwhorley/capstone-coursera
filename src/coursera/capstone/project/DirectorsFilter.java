6522ec683df86c1cdac4fa111aebe0781d1da71b
Greg Whorley <greg@whorley.com>
2016-02-14 13:50:21 -0800

    Fixed bug in DirectorsFilter
    Changed myDirectors field to String array and also changed satisfies method to iterate through the array.

--- src/coursera/capstone/project/DirectorsFilter.java
+++ src/coursera/capstone/project/DirectorsFilter.java
@@ -4,14 +4,19 @@ package coursera.capstone.project;
  * Created by greg on 2/10/2016.
  */
 public class DirectorsFilter implements Filter {
-    private String myDirectors;
+    private String[] myDirectors;
 
     public DirectorsFilter(String directors) {
-        myDirectors = directors;
+        myDirectors = directors.split(",");
     }
 
     @Override
     public boolean satisfies(String id) {
-        return myDirectors.contains(MovieDatabase.getDirector(id));
+        for (int index = 0; index < myDirectors.length; index++) {
+            if (MovieDatabase.getDirector(id).contains(myDirectors[index])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
